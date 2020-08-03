455f9ca1ee0a230e3ed00d05579c8a8159fea003
Dylychill <demckone@gmail.com>
2020-08-03 14:45:17 -0700

    random placement for bubbles maybe working

--- src/bubble.js
+++ src/bubble.js
@@ -4,23 +4,33 @@ function Bubble(props) {
     const [numBubbles, setNumBubbles] = useState(0)
     // below useEffect runs on first load to see if the game has been played before
     // updates the number of bubbles pressed
-    // BROKEN - not adding, it's doing strng concat
-    // useEffect(()=>{
-    //     const storedNum = localStorage.getItem('numBubbles')
-    //     if(storedNum) {
-    //       setNumBubbles(storedNum)
-    //       props.onSend(storedNum)
-    //     }
-    //   }, [])
+    // BROKEN - not adding, it's doing strng concat, to string may work
+  // useEffect(()=>{
+  //     const storedNum = localStorage.getItem('numBubbles')
+  //     if(storedNum) {
+  //       setNumBubbles(storedNum)
+  //       props.onSend(storedNum)
+  //     }
+  //   }, [])
 
-    return <div style={{position:"absolute"}}>
-        <img onClick = {() => {
-            props.onSend(numBubbles)
-            setNumBubbles(numBubbles+1)
-            //localStorage.setItem("numBubbles", numBubbles)
-        }}
-        src="/img/bubble.png" alt="a transparent, rainbow bubble"/>
-    </div>
+  // get random location and size for a new bubble - only need to change width cuz it is a square
+  // largest is should be is 300px by 300px and smallest is 50px
+  let size = Math.floor((Math.random() * 251) + 50);
+  // for position, get random top and left based on max
+  let height = 50;
+  let side = Math.floor((Math.random() * window.innerWidth));
+  console.log(side + "the side");
+  console.log(window.innerWidth + "window");
+
+  return <div className="bubble" style={{top:height + "px", left: side + "px"}}>
+    <img style={{width:size + "px"}}
+      onClick = {() => {
+        props.onSend(numBubbles)
+        setNumBubbles(numBubbles+1)
+        localStorage.setItem("numBubbles", numBubbles)
+    }}
+    src="/img/bubble.png" alt="a transparent, rainbow bubble"/>
+  </div>
 }
 
 export default Bubble;
