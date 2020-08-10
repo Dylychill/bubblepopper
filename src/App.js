a8122654777a7d9950daabb6be754234eb057dd9
Dylychill <demckone@gmail.com>
2020-08-09 19:38:53 -0700

    can change background on button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './App.css';
 import Bubble from './bubble.js';
 import Instructions from './instructions.js';
@@ -8,7 +8,8 @@ import Sidebar from './sidebar';
 function App(props) {
   // Start screen with instructions and a few bubbles, and then wait 15 seconds to start the game
   const [numBubbles, setNumBubbles] = useState(0)
-  console.log(numBubbles)
+  const inputRef = useRef()
+  //console.log(inputRef)
   // if already played, set the number - runs on load once
   useEffect(()=>{
     const storedNum = parseInt(localStorage.getItem('numBubbles'))
@@ -26,13 +27,13 @@ function App(props) {
   //   //return () => clearInterval(interval);
   // }, []);
 
-  return <div className="App">
+  return <div ref={inputRef} className="App">
     <h1>BUBBLE POPPER</h1>
     <Bubble onSend={setNumBubbles}></Bubble>
     <Bubble onSend={setNumBubbles}></Bubble>
     <Bubble onSend={setNumBubbles}></Bubble>
     <Bubble onSend={setNumBubbles}></Bubble>
-    <Sidebar num = {numBubbles}></Sidebar>
+    <Sidebar theApp={inputRef} num = {numBubbles}></Sidebar>
     <Instructions></Instructions>
   </div>
 }
--- src/sidebar.js
+++ src/sidebar.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 // Button api from: https://material-ui.com/components/buttons/
 
+// An array of possible background images
+const images = ["url('/img/gradient_lavender.png')","url('/img/gradient_orange&pink.png')","url('/img/gradient_orange&yellow.png')","url('/img/gradient_reds&oranges.png')"]
+
 function Sidebar(props) {
     // when the button is clicked, change the background color
     // This sidebar will always be on the screen
-
-    // sidebar is off by 1. it doesnt count the first bubble popped
     return <div className="sidebar">
       <p className="num_bubbles">{props.num}</p>
       <h2>Bubbles Popped</h2>
       <p>Tap the bubbles to pop them.</p>
-      <Button size="small" variant="contained" color="primary" onClick = {() => {
-        console.log("change background image of app.js")
-      }}>Change up the colors</Button>
+      <Button size="small" variant="contained" color="primary"
+        onClick = {() => {
+          // change the background image onclick randomly picking from the array above
+          props.theApp.current.style.backgroundImage = images[Math.floor(Math.random() * images.length)]
+        }
+      }>Change up the colors</Button>
 
     </div>
 }
