abfce05a63c236fae884b8b57fb425eddcd64971
Dylychill <demckone@gmail.com>
2020-08-10 12:13:32 -0700

    instructions disapears, changed font

--- src/instructions.js
+++ src/instructions.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Instructions() {
-  return <div className="instructions">
-    <p>This is a musical bubble popping game!</p>
-    <p>Click on the automatically generating bubbles to pop them and relax to music.</p>
-    <p>You can change the color palette to the left.</p>
+  const [show, setShow] = useState(true)
+  // Runs on first load - sets a timer to hide the instructions after 5 seconds
+  useEffect(()=>{
+    setTimeout(()=> {setShow(!show)}, 5000)
+  }, [])
+
+  return <div>
+    {show && <div className="instructions">
+      <p>This is a musical bubble popping game!</p>
+      <p>Click on the automatically generating bubbles to pop them and relax to music.</p>
+      <p>You can change the color palette to the left.</p>
+    </div>}
   </div>
 }
 
